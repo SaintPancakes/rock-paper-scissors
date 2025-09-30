573f09c991375ff803e9e45b03163ae17a7928a9
Tim Jackson <221983337+SaintPancakes@users.noreply.github.com>
2025-09-30 09:45:33 +0100

    computer chooses rock, paper or scissors

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function getComputerChoice(rps) {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+let choice = getComputerChoice()
+
+    if (choice === 1) {
+        alert ("Rock");
+    } else if (choice === 2) {
+        alert ("Paper");
+    } else {
+        alert ("Scissors")
+    }
+
